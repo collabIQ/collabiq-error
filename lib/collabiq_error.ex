42779b358bf2380e0bd2710e10e98c587ce9d8b7
Justin Coker <jcoker@JCoker-Air.local>
2020-02-09 21:33:35 -0600

    added test

--- lib/collabiq_error.ex
+++ lib/collabiq_error.ex
@@ -1,18 +1,58 @@
 defmodule CollabiqError do
-  @moduledoc """
-  Documentation for CollabiqError.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  # Handles multiple errors in a list and recursively sends
+  # the back through format/1
+  def format({:error, errors}) when is_list(errors) do
+    errors =
+      errors
+      |> Enum.map(&format/1)
 
-  ## Examples
-
-      iex> CollabiqError.hello()
-      :world
-
-  """
-  def hello do
-    :world
+    {:error, errors}
   end
+
+  def format({:error, error}), do: {:error, [format(error)]}
+
+  # Pattern matches errors produced by Ecto
+  def format({key, {msg, _opts}}) do
+    error = %{key: to_string(key), code: msg}
+    error |> lookup()
+  end
+
+  # Pattern matches most errors produced by CollabIQ
+  def format(%{key: key, code: code}) do
+    error = %{key: to_string(key), code: to_string(code)}
+    error |> lookup()
+  end
+
+  def format(error), do: error |> lookup()
+
+  defp lookup(%{key: key, code: "can't be blank"}),
+    do: %{message: "#{key} is required", code: "#{key}_required"}
+
+  defp lookup(%{key: key, code: "does not exist"}),
+    do: %{message: "#{key} was not found", code: "#{key}_not_found"}
+
+  defp lookup(%{key: key, code: "has already been taken"}),
+    do: %{message: "#{key} must be unique", code: "#{key}_unique"}
+
+  defp lookup(%{key: key, code: "invalid"}),
+    do: %{message: "#{key} is invalid", code: "#{key}_invalid"}
+
+  defp lookup(%{key: key, code: "is invalid"}),
+    do: %{message: "#{key} is invalid", code: "#{key}_invalid"}
+
+  defp lookup(%{key: key, code: "not_found"}),
+    do: %{message: "#{key} was not found", code: "#{key}_not_found"}
+
+  defp lookup(%{key: key, code: "required"}),
+    do: %{message: "#{key} is required", code: "#{key}_required"}
+
+  defp lookup(%{key: key, code: "unique"}),
+    do: %{message: "#{key} must be unique", code: "#{key}_unique"}
+
+  defp lookup(%{key: _key, code: _code}),
+    do: %{message: "something went wrong", code: "error"}
+
+  defp lookup(_), do: %{message: "something went wrong", code: "error"}
 end
--- test/collabiq_error_test.exs
+++ test/collabiq_error_test.exs
@@ -2,7 +2,65 @@ defmodule CollabiqErrorTest do
   use ExUnit.Case
   doctest CollabiqError
 
-  test "greets the world" do
-    assert CollabiqError.hello() == :world
+  test "standard errors" do
+    assert CollabiqError.format({:error, %{key: :site_id, code: "can't be blank"}}) ==
+             {:error, [%{message: "site_id is required", code: "site_id_required"}]}
+
+    assert CollabiqError.format({:error, %{key: :site_id, code: "does not exist"}}) ==
+              {:error, [%{message: "site_id was not found", code: "site_id_not_found"}]}
+
+    assert CollabiqError.format({:error, %{key: :site_id, code: "has already been taken"}}) ==
+      {:error, [%{message: "site_id must be unique", code: "site_id_unique"}]}
+
+    assert CollabiqError.format({:error, %{key: :site_id, code: "invalid"}}) ==
+      {:error, [%{message: "site_id is invalid", code: "site_id_invalid"}]}
+
+    assert CollabiqError.format({:error, %{key: :site_id, code: "is invalid"}}) ==
+      {:error, [%{message: "site_id is invalid", code: "site_id_invalid"}]}
+
+    assert CollabiqError.format({:error, %{key: :site_id, code: "not_found"}}) ==
+      {:error, [%{message: "site_id was not found", code: "site_id_not_found"}]}
+
+    assert CollabiqError.format({:error, %{key: :site_id, code: "required"}}) ==
+      {:error, [%{message: "site_id is required", code: "site_id_required"}]}
+
+    assert CollabiqError.format({:error, %{key: :site_id, code: "unique"}}) ==
+      {:error, [%{message: "site_id must be unique", code: "site_id_unique"}]}
+  end
+
+  test "list of standard errors" do
+    errors =
+      {:error,
+       [
+         %{key: :user_id, code: "does not exist"},
+         %{key: :site_id, code: "has already been taken"}
+       ]}
+
+    assert CollabiqError.format(errors) ==
+             {:error,
+              [
+                %{code: "user_id_not_found", message: "user_id was not found"},
+                %{code: "site_id_unique", message: "site_id must be unique"}
+              ]}
+  end
+
+  test "standard error with string key" do
+    assert CollabiqError.format({:error, %{key: "site_id", code: "can't be blank"}}) ==
+             {:error, [%{message: "site_id is required", code: "site_id_required"}]}
+  end
+
+  test "error without map" do
+    assert CollabiqError.format({:error, "blah"}) ==
+      {:error, [%{message: "something went wrong", code: "error"}]}
+  end
+
+  test "changeset errors" do
+    assert CollabiqError.format({:error, [name: {"can't be blank", [validation: :required]}]}) ==
+      {:error, [%{message: "name is required", code: "name_required"}]}
+  end
+
+  test "error with unknown message" do
+    assert CollabiqError.format({:error, %{key: :site_id, code: "blah"}}) ==
+             {:error, [%{message: "something went wrong", code: "error"}]}
   end
 end
