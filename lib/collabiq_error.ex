e46d7c240b7bd5ea3440105b14c266181d5ba9cc
Justin Coker <jcoker@JCoker-Air.local>
2020-02-09 20:46:07 -0600

    first commit

---
+++ lib/collabiq_error.ex
@@ -0,0 +1,18 @@
+defmodule CollabiqError do
+  @moduledoc """
+  Documentation for CollabiqError.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CollabiqError.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/collabiq_error_test.exs
@@ -0,0 +1,8 @@
+defmodule CollabiqErrorTest do
+  use ExUnit.Case
+  doctest CollabiqError
+
+  test "greets the world" do
+    assert CollabiqError.hello() == :world
+  end
+end
